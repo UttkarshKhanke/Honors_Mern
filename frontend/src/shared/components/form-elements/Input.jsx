1758c9105b0dd803b46835ead3e5706e062a2d57
UttkarshKhanke <uttkarsh.khanke@gmail.com>
2024-05-08 18:56:20 +0530

    Login Auth Update

--- frontend/src/shared/components/form-elements/Input.jsx
+++ frontend/src/shared/components/form-elements/Input.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useReducer } from "react";
 import { validate } from '../../utils/validators';
-
 const inputReducer = (state, action) => {
     switch(action.type) {
         case 'CHANGE':
@@ -21,19 +20,25 @@ const inputReducer = (state, action) => {
 };
 
 const Input = props => {
+    console.log('Input component called ' + new Date())
     const [inputState, dispatch] = useReducer(inputReducer, { 
         value: props.initialValue || '', 
         isValid: props.initialIsValid || false,
         isTouched: false,
     });
+    console.log(inputState);
     const { id, onInput } = props;
     const { value, isValid } = inputState;
+
     useEffect(() => {
         onInput(id, value, isValid);
     }, [id, value, isValid, onInput]);
+
     const changeHandler = event => {
+        console.log('change hander called ' + new Date())
         dispatch({type: 'CHANGE', val: event.target.value, validators: props.validators});
     };
+    console.log('changeHander', {changeHandler});
     const touchHandler = event => {
         dispatch({type: 'TOUCH', val: event.target.value});
     };
@@ -57,7 +62,6 @@ const Input = props => {
         value={inputState.value}
     />
     );
-
     return (
         <div className="mb-4">
             <label htmlFor={props.id} className="block text-gray-700 text-sm font-bold mb-2">{props.label}</label>
@@ -66,5 +70,4 @@ const Input = props => {
         </div>
     );
 };
-
 export default Input;
